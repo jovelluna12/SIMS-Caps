eb2db57526076c6ad328b67696ee2637ef62b801
jovelluna <jchrister718@gmail.com>
2022-09-07 16:26:11 +0800

    changes

--- Manager.py
+++ Manager.py
@@ -2,25 +2,28 @@ import Employee
 import dbConnector
 class Manager (Employee.Employee):
 
-    def approveEmployee(self,AppID,status):
+    def approveEmp(self,AppID,status):
         print("approve employee")
 
-    def viewInventory(self):
+    def viewInv(self):
         print("Viewing Inventory")
 
-    def viewSalesTransaction(self):
+    def viewSales(self):
         print("viewing Sales Transaction")
 
-    def AddEmployees(self,EmpID,name, username, password, role):
+    def AddEmp(self,EmpID,name, username, password, role):
         dbcursor =  dbConnector.db.cursor()
-        self.EmpID=EmpID
-        self.name=name
-        self.username=username
-        self.password=password
-        self.role=role
         query="INSERT INTO employees (EmpID,name,username,password,role) VALUES(%s,%s,%s,%s,%s)"
-        value=(self.EmpID,self.name,self.username,self.password,self.role)
+        value=(EmpID,name,username,password,role)
         dbcursor.execute(query,value)
+        dbcursor.close()
+        dbConnector.db.commit()
+        dbConnector.db.close()
+
+    def AddEmpMany(self,val):
+        dbcursor = dbConnector.db.cursor()
+        query = "INSERT INTO employees (EmpID,name,username,password,role) VALUES(%s,%s,%s,%s,%s)"
+        dbcursor.executemany(query,val)
         dbcursor.close()
         dbConnector.db.commit()
         dbConnector.db.close()
--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 import Employee, Manager, Owner, Product
 
 x=Manager.Manager()
-x.AddEmployees(12,'christer','chris','password','Cashier')
+
+# x.AddEmp(12,'christer','chris','password','Cashier')
+
+# val = [
+#   (11,'jovel','jluna','pass','Cashier'),
+#   (12,'apple','app','pass','Cashier'),
+#   (13,'burgers','jluna','pass','Cashier'),
+#   (14,'richk','jluna','pass','Cashier'),
+#   (15,'unknwa','jluna','pass','Cashier'),
+#   (16,'jovsadel','jluna','pass','Cashier'),
+#
+# ]
+# x.AddEmpMany(val)
+y=Product.product("Paracetamol","50","Fresh",5.00)
+y.add("Paracetamol","50","Fresh",5.00)
+
 
