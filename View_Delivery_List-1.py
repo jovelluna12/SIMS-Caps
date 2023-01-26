7dcabd068838baf70854c4a1b92b781451988513
UyaoJan <79205692+UyaoJan@users.noreply.github.com>
2023-01-26 14:43:06 +0800

    this is the new update of the VDL in 1/26/23 by Uyao

--- View_Delivery_List-1.py
+++ View_Delivery_List-1.py
@@ -9,14 +9,15 @@ from datetime import datetime
 
 class VDL:
     def __init__(self):
+        global root
         
         root = tk.Tk()
         root.title("Delivery Detail")
-        width=1000
-        height=730
+        width=950
+        height=630
         screenwidth = root.winfo_screenwidth()
         screenheight = root.winfo_screenheight()
-        # root.protocol("WM_DELETE_WINDOW", self.on_Closing)
+        root.protocol("WM_DELETE_WINDOW", self.on_Closing)
         alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
         root.geometry(alignstr)
         root.resizable(width=False, height=False)
@@ -27,7 +28,7 @@ class VDL:
         Adrress=tk.Label(root,text="Ilaya Carmen, Cagayan de Oro City",font=('Arial',15),justify=LEFT)
         Adrress.place(x=10,y=85)
 
-        Dstore=tk.Label(root,text="Delivery Store:",font=('Arial',12),justify=LEFT)
+        Dstore=tk.Label(root,text="Purchase Order:",font=('Arial',12),justify=LEFT)
         Dstore.place(x=10,y=115)
 
         DateAr=tk.Label(root,text="Date Arravil:",font=('Arial',12),justify=LEFT)
@@ -42,15 +43,17 @@ class VDL:
 
         global QTYIN_Table
         Table=Frame(root)
-        Table.place(x=10,y=140,width=763,height=300)
+        Table.place(x=10,y=140,width=922,height=300)
         QTYIN_Table = ttk.Treeview(Table, height=25)
-        QTYIN_Table['columns'] = ("ID","Name", "Price","QTY","QTYIN","TotalQTY","TotalPrice")
+        QTYIN_Table['columns'] = ("ID","Name", "Price","QTY","QTYIN","QTYOUT","MARK","TotalQTY","TotalPrice")
         QTYIN_Table.column("#0", width=0, stretch=NO)
         QTYIN_Table.column("ID",anchor=W,width=80,stretch=NO)
         QTYIN_Table.column("Name",anchor=W,width=360,stretch=NO)
         QTYIN_Table.column("Price",anchor=CENTER,width=80,stretch=NO)
         QTYIN_Table.column("QTY",anchor=CENTER,width=40,stretch=NO)
         QTYIN_Table.column("QTYIN",anchor=CENTER,width=60,stretch=NO)
+        QTYIN_Table.column("QTYOUT",anchor=CENTER,width=60,stretch=NO)
+        QTYIN_Table.column("MARK",anchor=CENTER,width=100,stretch=NO)
         QTYIN_Table.column("TotalQTY",anchor=CENTER,width=60,stretch=NO)
         QTYIN_Table.column("TotalPrice",anchor=E,width=65,stretch=NO)
 
@@ -61,79 +64,61 @@ class VDL:
         QTYIN_Table.heading("Price", text="Unit Price", anchor=CENTER)
         QTYIN_Table.heading("QTY", text="QTY", anchor=CENTER)
         QTYIN_Table.heading("QTYIN", text="QTY IN", anchor=CENTER)
+        QTYIN_Table.heading("QTYOUT", text="QTY OUT", anchor=CENTER)
+        QTYIN_Table.heading("MARK", text="MARK", anchor=W)
         QTYIN_Table.heading("TotalQTY", text="Total QTY", anchor=CENTER)
-        QTYIN_Table.heading("TotalPrice", text="Total Price", anchor=E)
+        QTYIN_Table.heading("TotalPrice", text="Total Price", anchor=W)
         scrollbar = ttk.Scrollbar(Table, orient="vertical", command=QTYIN_Table.yview)
         scrollbar.place(relx=1.0, rely=0.0, anchor="ne")
         QTYIN_Table.configure(yscrollcommand=scrollbar.set)
         QTYIN_Table.pack(expand=1,fill=BOTH)
 
-        Retrun=tk.Label(root,text="Product To Retrun:",font=('Arial',12),justify=LEFT)
-        Retrun.place(x=10,y=444)
-
-        global QTYOUT_Table
-        TableOUT=Frame(root)
-        TableOUT.place(x=10,y=470,width=863,height=250)
-        QTYOUT_Table = ttk.Treeview(TableOUT, height=25)
-        QTYOUT_Table['columns'] = ("ID","Name", "Price","QTY","QTYOUT","TotalQTY","TotalPrice","MARK")
-        QTYOUT_Table.column("#0", width=0, stretch=NO)
-        QTYOUT_Table.column("ID",anchor=W,width=80,stretch=NO)
-        QTYOUT_Table.column("Name",anchor=W,width=360,stretch=NO)
-        QTYOUT_Table.column("Price",anchor=CENTER,width=80,stretch=NO)
-        QTYOUT_Table.column("QTY",anchor=CENTER,width=40,stretch=NO)
-        QTYOUT_Table.column("QTYOUT",anchor=CENTER,width=60,stretch=NO)
-        QTYOUT_Table.column("TotalQTY",anchor=CENTER,width=60,stretch=NO)
-        QTYOUT_Table.column("TotalPrice",anchor=E,width=65,stretch=NO)
-        QTYOUT_Table.column("MARK",anchor=E,width=100,stretch=NO)
-
-        # TableOUT Head
-        QTYOUT_Table.heading("#0")
-        QTYOUT_Table.heading("ID", text="ID", anchor=W)
-        QTYOUT_Table.heading("Name", text="Product", anchor=W)
-        QTYOUT_Table.heading("Price", text="Unit Price", anchor=CENTER)
-        QTYOUT_Table.heading("QTY", text="QTY", anchor=CENTER)
-        QTYOUT_Table.heading("QTYOUT", text="QTY OUT", anchor=CENTER)
-        QTYOUT_Table.heading("TotalQTY", text="Total QTY", anchor=CENTER)
-        QTYOUT_Table.heading("TotalPrice", text="Total Price", anchor=CENTER)
-        QTYOUT_Table.heading("MARK", text="Mark", anchor=W)
-        scrollbar = ttk.Scrollbar(TableOUT, orient="vertical", command=QTYOUT_Table.yview)
-        scrollbar.place(relx=1.0, rely=0.0, anchor="ne")
-        QTYOUT_Table.configure(yscrollcommand=scrollbar.set)
-        QTYOUT_Table.pack(expand=1,fill=BOTH)
-
         emp=Employee.Employee()
         # CashName=emp.getEmployee_Name()
-        GLabel_450=tk.Label(root,text="Cashier: ",font=('Arial',15),justify=LEFT)
-        GLabel_450.place(x=780,y=240)
+        GLabel_450=tk.Label(root,text="Name: ",font=('Arial',15),justify=LEFT)
+        GLabel_450.place(x=600,y=440)
 
         GLabel_170=tk.Label(root,text="Subtotal: PHP ",font=('Arial',10),justify=LEFT)
-        GLabel_170.place(x=780,y=270)
+        GLabel_170.place(x=600,y=470)
 
         # VAT=0.12*subtotal
         
         GLabel_139=tk.Label(root,text="12% VAT: PHP",font=('Arial',10),justify=LEFT)
-        GLabel_139.place(x=780,y=290)
-
-        GLabel_522=tk.Label(root,text="Senior/PWD Discount: PHP",font=('Arial',10),justify=LEFT)
-        GLabel_522.place(x=780,y=310)
+        GLabel_139.place(x=600,y=490)
 
         GLabel_523=tk.Label(root,text="LESS: Other Discounts:",font=('Arial',10),justify=LEFT)
-        GLabel_523.place(x=780,y=330)
-
-        # global finalprice
-        # finalprice=subtotal-disc1-disc
+        GLabel_523.place(x=600,y=510)
 
         GLabel_524=tk.Label(root,text="Total Amount Due ",font=('Arial',10),justify=LEFT)
-        GLabel_524.place(x=780,y=350)
+        GLabel_524.place(x=600,y=530)
 
-        GLabel_525=tk.Label(root,text="Cash: ",font=('Arial',10),justify=LEFT)
-        GLabel_525.place(x=780,y=370)            
+        #VENDOR INFORMATION>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+        emp=Employee.Employee()
+        # CashName=emp.getEmployee_Name()
+        Vendor_ID=tk.Label(root,text="ID: ",font=('Arial',15),justify=LEFT)
+        Vendor_ID.place(x=10,y=440)
 
-        GLabel_526=tk.Label(root,text="Change: ",font=('Arial',10),justify=LEFT)
-        GLabel_526.place(x=780,y=390)
+        Vendor_Name=tk.Label(root,text="Name:",font=('Arial',10),justify=LEFT)
+        Vendor_Name.place(x=10,y=470)
+        
+        Vendor_Address=tk.Label(root,text="Address:",font=('Arial',10),justify=LEFT)
+        Vendor_Address.place(x=10,y=490)
 
+        Vendor_Contact=tk.Label(root,text="Contact Number:",font=('Arial',10),justify=LEFT)
+        Vendor_Contact.place(x=10,y=510)
+
+        Vendor_Email=tk.Label(root,text="Email:",font=('Arial',10),justify=LEFT)
+        Vendor_Email.place(x=10,y=530)
+
+        Vendor_Ship=tk.Label(root,text="Total Amount Shipping: ",font=('Arial',10),justify=LEFT)
+        Vendor_Ship.place(x=10,y=550)
 
         root.mainloop()
+    
+    def on_Closing(self):
+        if messagebox.askyesno("Warning","Closing this Window this Transaction.\nContinue Closing?"):
+            root.destroy()
+
 
 if __name__ == "__main__":
     vdl = VDL()
